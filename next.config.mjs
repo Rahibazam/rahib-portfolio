e94c521619bb7f885f10c16840cfe673735c49ce
Rahib Azam <128047222+Rahibazam@users.noreply.github.com>
2026-09-20 23:07:22 +0600

    Redirect Vercel hostname to custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'rahib-azam.vercel.app'
+          }
+        ],
+        destination: 'https://www.rahibazam.com/:path*',
+        permanent: true
+      }
+    ];
+  },
   images: {
     formats: ['image/avif', 'image/webp']
   }
